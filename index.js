0ec06af14a677a4a2d0102e7d526dbc3766625ec
Michael <michaelzmyers@gmail.com>
2021-10-23 22:30:49 -0500

    setting a port up

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const chalk = require('chalk')
 const http = require('./src/app').http;
 
 const port = process.env.PORT || 5001
+console.log(`THE PORT IS ${port}`)
 
 http.listen(port, () => {
     console.log(`Server ${chalk.green('connected')}: ${chalk.magenta(new Date().toISOString())}`)
